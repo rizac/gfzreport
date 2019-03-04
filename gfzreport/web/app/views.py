af82951c0d72dc07e6bc58f86272a5310c2063f4
rizac <riccardo.zaccarelli@gfz-potsdam.de>
2019-03-04 14:16:40 +0100

    hiding path in home.html

--- gfzreport/web/app/views.py
+++ gfzreport/web/app/views.py
@@ -57,7 +57,7 @@ def handle_invalid_usage(error):
 @mainpage.route('/')
 def index():
     return render_template("home.html",
-                           title=current_app.config['DATA_PATH'],
+                           title=os.path.basename(current_app.config['DATA_PATH']),
                            reports=get_reports(current_app))
 
 
